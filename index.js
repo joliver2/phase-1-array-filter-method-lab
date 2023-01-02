98a83c50cbf225b22da3d79fba4446f401b9fe18
Jeremy Oliver <joliver427@icloud.com>
2023-01-02 10:04:47 -0700

    completed commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driverArray, string) {
+    return driverArray.filter(function (string1) {
+        return string1.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driverArray, string) {
+    return driverArray.filter(function (string1) {
+        return string.charAt(0) === string1.charAt(0)
+    })
+}
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
